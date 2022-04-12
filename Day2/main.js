638566313dee841df39ae770b4382ff080ff50f0
Lauren <ls_garwood@yahoo.co.uk>
2022-04-12 09:22:56 +0100

    LG: First commit day 2 intial files

---
+++ Day2/main.js
@@ -0,0 +1,9 @@
+// JavaScript != Java
+// JavaScripts syntax at first may resemble but they are different
+// Primarily front end tool
+
+//Browser, inline, externally
+
+console.log("Hello World!")
+
+
