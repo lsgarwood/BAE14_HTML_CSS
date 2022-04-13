802a0e520da3f044f2bde3c6a33c419fcdd88e5c
Lauren <ls_garwood@yahoo.co.uk>
2022-04-13 16:53:43 +0100

    LG:Clicker changes and linking css

--- Day 3/app.js
+++ Day 3/app.js
@@ -1,10 +1,10 @@
 'use strict'
 //query selectors
-const titleEl = document.getElementById("title-el");
-const countEl = document.getElementById("count-el");
-const addOneEl = document.getElementById("plusone-el");
-const saveEl = document.getElementById("save-el");
-const HistoryEl = document.getElementById("history");
+const titleEl = document.querySelector("#title-el");
+const countEl = document.querySelector("#count-el");
+const addOneEl = document.querySelector("#plusone-el");
+const saveEl = document.querySelector("#save-el");
+const historyEl = document.querySelector("#history-el");
 
 //variables
 let count = 0;
@@ -13,17 +13,31 @@ let saveHistory = []
 //functions
 function increment() {
     count ++;
-    countEl.textContent = count;
-    console.log("we work"); //InnerHTML, TextContent, InnerText
+    countEl.textContent = count; //InnerHTML, TextContent, InnerText
+    console.log("we work"); 
 }
 
 function save() {
     saveHistory.push(count);
-    countEl.textContent = count;
-    HistoryEl.textContent = saveHistory;
-    console.log("we save");
+
+    const li = document.createElement("li");
+    li.textContent = count;
+    historyEl.appendChild(li);
+    console.log("now we save");
     count = 0;
     countEl.textContent = count;
+
+    //historyEl.innerHTML += "<li>" + count + "<li/>";
+
+    // for (let i = 0; i < saveHistory.length; i++) {
+    //     historyEl.innerHTML += "<li>" + saveHistory[i] + "</li>"
+    // }
+
+    // countEl.textContent = count;
+    // historyEl.textContent = saveHistory;
+    // console.log("now we save");
+    // count = 0;
+    // countEl.textContent = count;
 }
 
 //connect queries to functions in final event listeners
